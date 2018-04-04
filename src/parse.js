27caf81e753872b72c927bb4bbfb8ede882b8170
romanoffivan <iw.romanof@yandex.ru>
2018-04-04 16:18:36 +0300

    update parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,10 @@
 import { safeLoad } from 'js-yaml';
+import ini from 'ini';
 
 const propertyActions = {
   '.json': type => JSON.parse(type),
   '.yaml': type => safeLoad(type),
+  '.ini': type => ini.parse(type),
 };
 
 export default arg => propertyActions[arg];
