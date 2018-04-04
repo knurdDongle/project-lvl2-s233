905da2339e581d1b60890a53d0ef67965c8c1974
romanoffivan <iw.romanof@yandex.ru>
2018-04-04 13:01:19 +0300

    creat tests for yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,5 @@
 import genDiff from '../src/';
 
-const path1 = '__tests__/__fixtures__/before.json';
-const path2 = '__tests__/__fixtures__/after.json';
-
 const expected = `{
     host: hexlet.io
   + timeout: 20
@@ -11,6 +8,17 @@ const expected = `{
   + verbose: true
 }`;
 
-test('genDiff', () => {
-  expect(genDiff(path1, path2)).toBe(expected);
+describe('difference', () => {
+  it('json', () => {
+    const path1 = '__tests__/__fixtures__/before.json';
+    const path2 = '__tests__/__fixtures__/after.json';
+    const result = genDiff(path1, path2);
+    expect(result).toBe(expected);
+  });
+  it('yaml', () => {
+    const path1 = '__tests__/__fixtures__/before.yml';
+    const path2 = '__tests__/__fixtures__/after.yml';
+    const result = genDiff(path1, path2);
+    expect(result).toBe(expected);
+  });
 });
