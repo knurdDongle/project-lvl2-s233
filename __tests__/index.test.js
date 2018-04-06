7e22861eda824d91e0c4636be2f8ce56c007e983
romanoffivan <iw.romanof@yandex.ru>
2018-04-06 19:52:35 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,9 +4,26 @@ import genDiff from '../src/';
 describe('difference', () => {
   const pathToResult1 = '__tests__/__fixtures__/flat/result';
   const pathToResult2 = '__tests__/__fixtures__/nested/result';
+  const pathToResult3 = '__tests__/__fixtures__/result.plan';
   const expected1 = fs.readFileSync(pathToResult1, 'utf-8');
   const expected2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const expected3 = fs.readFileSync(pathToResult3, 'utf-8');
 
+  it('plan json', () => {
+    const path1 = '__tests__/__fixtures__/flat/before.json';
+    const path2 = '__tests__/__fixtures__/flat/after.json';
+    expect(genDiff(path1, path2)).toBe(expected3);
+  });
+  it('plan yaml', () => {
+    const path1 = '__tests__/__fixtures__/flat/before.yaml';
+    const path2 = '__tests__/__fixtures__/flat/after.yaml';
+    expect(genDiff(path1, path2)).toBe(expected3);
+  });
+  it('plan ini', () => {
+    const path1 = '__tests__/__fixtures__/flat/before.ini';
+    const path2 = '__tests__/__fixtures__/flat/after.ini';
+    expect(genDiff(path1, path2)).toBe(expected3);
+  });
   it('json', () => {
     const path1 = '__tests__/__fixtures__/flat/before.json';
     const path2 = '__tests__/__fixtures__/flat/after.json';
@@ -23,17 +40,17 @@ describe('difference', () => {
     expect(genDiff(path1, path2)).toBe(expected1);
   });
 
-  it('json1', () => {
+  it('json tree', () => {
     const path1 = '__tests__/__fixtures__/nested/before.json';
     const path2 = '__tests__/__fixtures__/nested/after.json';
     expect(genDiff(path1, path2)).toBe(expected2);
   });
-  it('yaml1', () => {
+  it('yaml tree', () => {
     const path1 = '__tests__/__fixtures__/nested/before.yaml';
     const path2 = '__tests__/__fixtures__/nested/after.yaml';
     expect(genDiff(path1, path2)).toBe(expected2);
   });
-  it('ini1', () => {
+  it('ini1 tree', () => {
     const path1 = '__tests__/__fixtures__/nested/before.ini';
     const path2 = '__tests__/__fixtures__/nested/after.ini';
     expect(genDiff(path1, path2)).toBe(expected2);
